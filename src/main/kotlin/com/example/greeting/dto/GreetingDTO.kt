2d6f65f568dec97e171defec97f2893aeb309c0a
york <york.tsai@fundroots.com>
2022-06-11 11:08:43 +0800

    Return without null value

--- src/main/kotlin/com/example/greeting/dto/GreetingDTO.kt
+++ src/main/kotlin/com/example/greeting/dto/GreetingDTO.kt
@@ -1,5 +1,8 @@
 package com.example.greeting.dto
 
+import com.fasterxml.jackson.annotation.JsonInclude
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 data class GreetingDTO(
     val subject: String,
     val content: String,
